59a5a6f43b90a6221337eaa0b22e5a6d101430a1
lamlb <lam.lb215073@sis.hust.edu.vn>
2023-10-11 22:17:54 +0700

    First Dialog ex

---
+++ FirstDialog.java
@@ -0,0 +1,8 @@
+//Example 2: FirstDialog.log
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    public static void main(String[] args){
+        JOptionPane.showMessagDialog(null,"Hello world! How are you");
+        System.exit(0);
+    }
+}
